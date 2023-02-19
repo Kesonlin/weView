3fda84f8c5757747691e7ff341556740ae0f23bb
曾智阳 <zzy18218203446@163.com>
2023-02-19 14:46:47 +0800

    fix: 修改部分路径

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,15 +1,24 @@
-import { App } from "vue";
-import DateTimePicker from "./DatePicker/DateTimePicker/DateTimePicker.vue";
-// import Button from "./Button/src/Button.vue";
+import { App } from 'vue'
+import DateTimePicker from './DatePicker/DateTimePicker/DateTimePicker.vue'
+import Input from './input/Input.vue'
+import Pagination from './Pagination/index'
+import Button from "./Button/src/Button.vue";
+import Col from './Layout/src/col.vue';
+import Row from './Layout/src/row.vue'
+import Notification from './notification/Notification.vue'
 
 // 导出单独组件
-export { DateTimePicker };
-
+export { DateTimePicker, Input, Pagination,Button,Col,Row,Notification }
 // 编写一个插件，实现一个install方法
 
 export default {
   install(app: App): void {
-    app.component("DateTimePicker", DateTimePicker);
-    // app.component(Button.name, Button);
+    app.component('DateTimePicker', DateTimePicker);
+    app.component(Input.name, Input);
+    app.component('Pagination', Pagination);
+    app.component('WButton', Button);
+    app.component('WRow',Row);
+    app.component('WCol',Col);
+    app.component(Notification.name,Notification)
   },
-};
+}
